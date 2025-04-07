2e9aee923827c9b3a2ebeeaabc81645149ae121b
Khang Lê <duykhang12a7@gmail.com>
2025-04-07 12:27:13 +0700

    fix tailwind imports in index.css

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    cssMinify: 'lightningcss',
     rollupOptions: {
       output: {
         manualChunks: undefined
